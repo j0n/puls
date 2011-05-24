fa5fe5cd33d718d0ac110b91882df9b631059b61
Jon Andersson <andersson.jon@gmail.com>
2011-05-24 11:58:20 +0200

    four 4square connected

--- js/an.js
+++ js/an.js
@@ -1,41 +1,80 @@
 (function(c){var b=c.setTimeout,d=c.document,a=0;c.jXHR=function(){var e,g,n,h,m=null;function l(){try{h.parentNode.removeChild(h)}catch(o){}}function k(){g=false;e="";l();h=null;i(0)}function f(p){try{m.onerror.call(m,p,e)}catch(o){throw new Error(p)}}function j(){if((this.readyState&&this.readyState!=="complete"&&this.readyState!=="loaded")||g){return}this.onload=this.onreadystatechange=null;g=true;if(m.readyState!==4){f("Script failed to load ["+e+"].")}l()}function i(o,p){p=p||[];m.readyState=o;if(typeof m.onreadystatechange==="function"){m.onreadystatechange.apply(m,p)}}m={onerror:null,onreadystatechange:null,readyState:0,open:function(p,o){k();internal_callback="cb"+(a++);(function(q){c.jXHR[q]=function(){try{i.call(m,4,arguments)}catch(r){m.readyState=-1;f("Script failed to run ["+e+"].")}c.jXHR[q]=null}})(internal_callback);e=o.replace(/=\?/,"=jXHR."+internal_callback);i(1)},send:function(){b(function(){h=d.createElement("script");h.setAttribute("type","text/javascript");h.onload=h.onreadystatechange=function(){j.call(h)};h.setAttribute("src",e);d.getElementsByTagName("head")[0].appendChild(h)},0);i(2)},setRequestHeader:function(){},getResponseHeader:function(){return""},getAllResponseHeaders:function(){return[]}};k();return m}})(window);
 var persCount = 0;
 window.onload = function(){
-/*   var c = document.getElementById('c');
-   if (c.getContext){
-               var ctx = c.getContext('2d');
-               ctx.fillRect(25,25,100,100);
-               ctx.clearRect(45,45,60,60);
-               ctx.strokeRect(50,50,50,50);
-   }
-   for(var i = 0; i<10;i++){
-       var t = i * 1000;
-   }*/
-    setInterval(checkCheckins, 5000);
-		var places = {
-						'1':{
-							'url':'https://api.foursquare.com/v2/venues/738899/herenow?oauth_token=Y5HZMBX20YYLNU5P34GCC40G3SFFTUMK40SBGLWCKEU4EE1R',
-							'count':0
+		var places = [
+						{
+                            'name':'solde',
+							'url':'https://api.foursquare.com/v2/venues/241005/herenow?oauth_token=Y5HZMBX20YYLNU5P34GCC40G3SFFTUMK40SBGLWCKEU4EE1R',
+							'count':0,
+                            'color':0x777FFF00
+
 						},
-						'2':{
-							'url': 'https://api.foursquare.com/v2/venues/4af565bbf964a52005f921e3/herenow?oauth_token=Y5HZMBX20YYLNU5P34GCC40G3SFFTUMK40SBGLWCKEU4EE1R',
-							'count':0
+						{
+                            'name':'kaffebar',
+							'url': 'https://api.foursquare.com/v2/venues/13781349/herenow?oauth_token=Y5HZMBX20YYLNU5P34GCC40G3SFFTUMK40SBGLWCKEU4EE1R',
+							'count':0,
+                            'color':0xAAAA0099
+						},
+						{
+                            'name':'triangel stationen',
+							'url': 'https://api.foursquare.com/v2/venues/13690111/herenow?oauth_token=Y5HZMBX20YYLNU5P34GCC40G3SFFTUMK40SBGLWCKEU4EE1R',
+							'count':0,
+                            'color':0xAA228429
+						},
+						{
+                            'name':'Centralen',
+							'url': 'https://api.foursquare.com/v2/venues/223593/herenow?oauth_token=Y5HZMBX20YYLNU5P34GCC40G3SFFTUMK40SBGLWCKEU4EE1R',
+							'count':0,
+                            'color':0x99228429
 						}
-		}
-}]
+		];
+        window.puls = new window.puls.View(places);
 };
 (function(){
-	
-})();
-function checkCheckins(url){
-        var x1 = new jXHR();
-        x1.onerror = function(msg, url){alert(msg);};
-        x1.onreadystatechange = function(data){
-              if (x1.readyState === 4) {
-//                  persCount = data.response.hereNow.count;
-                  //document.getElementById('count').
+    puls = {};	
+    puls.View = function(data){
+        this.places = data;
+        this.lastChecked = 0;
+        var self = this;
+        check = function(){
+           self.lastChecked = self.places.length != self.lastChecked+1 ? self.lastChecked+1 : 0; 
+           f4call(self.lastChecked);
+        }
+        f4call = function(index){
+            var x1 = new jXHR();
+            var current = self.places[index];
+            x1.onerror = function(msg, url){};
+            x1.onreadystatechange = function(data){
+                  if (x1.readyState === 4) {
+                  console.log(current.name, data.response.hereNow.count);
+                    if(current.count != data.response.hereNow.count){
+                        update(current, data.response.hereNow.count);
+                    }
+                }
+            };
+            x1.open("GET",current.url + '&callback=?');
+            x1.send();
+        }
+
+        update = function(obj, count){
+            obj.count = count;
+            window.updateCurve(obj.name,count);
+        }
+
+        this.init = function(){
+            for(var  i=0, ii = self.places.length;i<ii;i++){
+                var p = self.places[i];
+                window.addCurve(p.name, p.count,p.color);
+                //f4call(i);
             }
-        };
-        x1.open("GET",url + '&callback=?');
-        x1.send();
-}
+            setInterval(check, 10000);
+        }
+        this.addPlace = function(key, data){
+           this.places[key] = data; 
+        }
+        this.getPlaces = function(){
+            return self.places;
+        }
+    };
+    window.puls = puls;
+})();
--- js/p.pde
+++ js/p.pde
@@ -3,7 +3,6 @@ int w;              // Width of entire wave
 
 float theta = 0.0;       // Start angle at 0
 float period = 1500.0;    // How many pixels before the wave repeats
-float dx;                 // Value for incrementing X, to be calculated as a function of period and xspacing
 float[] yvalues;          // Using an array to store height values for the wave (not entirely necessary)
 var currentPersCount = 0;
 var curves = {};
@@ -18,9 +17,6 @@ void setup() {
   smooth();
   w = width+16;
   yvalues = new float[w/xspacing];
-	addCurve('solde',5, 1655, 200);
-	addCurve('njutbar',25, 2800, 12);
-	addCurve('kaffebar',1, 700, 120);
 }
 
 
@@ -30,63 +26,69 @@ void draw() {
 		calculateCurve(c);
 	}
 }
-void addCurve(key, count,p,c ){
-	curves[key] = {'current':count, 'next': count, 'period':p, 'color':c};
+void addCurve(key, count,c){
+	curves[key] = {
+        'current':count,
+        'kk':0,
+        'color':c
+    };
+    updateCurve(key, count);
 }
 void calculateCurve(key){
-	var c = curves[key];
-	var k, treshold,change, cp, p;
-	var cp = c.next;
-	var p = c.current;
-	var pe = c.period;
+  var c = curves[key];
+  var k = c.kk;
 
-  if(cp != p && p != 0){
-      cp = p;
-      change = k > 0 ? -(cp*0.9) : cp*0.9;
-			//recaluate
+  if(k > c.threshold){
+    c.change = -(c.count*0.3);
+    k = c.threshold;
   }
-
-  if(cp == 0){ cp = 1;}
-  treshold = cp * 5;
-
-  if(k > treshold){
-    change = -(cp*0.3);
-    k = treshold;
-  }
-  else if(k < -(treshold)){
-    change = (cp*0.3);
-    k = -treshold;
+  else if(k < -(c.threshold)){
+    c.change = (c.count*0.3);
+    k = -c.threshold;
   }
   else{
-     k = k + change;
+     k = k + c.change;
   }
-  var speed = (cp)*2/30000;
-  var yT = cp/100;//y treshold
-  dx = (TWO_PI / pe) * xspacing;
+
   for(var i = 0;i<10;i++){
-      calcWave(speed*i, k+(i*(cp*0.4)));
-      renderWave(c.color);
+      calcWave(c.speed*i, k+(i*(c.count*0.4)),c.dx);
+      renderWave(c.color,c.alpha);
   }
+  c.kk = k;
 }
 
-void calcWave(v, amp) {
+void updateCurve(key, count){
+    console.log('update', count);
+    count = (count+1)*3;
+    var c = curves[key];
+    c.count = count;
+    c.threshold = count * 5;
+    c.speed = count*2/30000;
+    c.period = 700 + count * 170 + Math.floor(Math.random(10)*1000);
+    c.dx = (TWO_PI / c.period) * xspacing;
+    c.change = c.change > 0 ? -(count*0.9) : count*0.9;
+    c.alpha = count < 10 ? 20 : 30;
+}
+
+void calcWave(v, amp, dx) {
   theta += v;
   float x = theta;
   for (int i = 0; i < yvalues.length; i++) {
-    yvalues[i] = sin(x)*amp-420;
+    yvalues[i] = sin(x)*amp-220;
     x+=dx;
   }
 }
 
-void renderWave(i) {
+void renderWave(c, a) {
+    //console.log(c);
   for (int x = 0; x < yvalues.length; x++) {
     noStroke();
-    //var i = mouseX/4;
-    i = Math.floor(i);
-    colorMode(RGB,30,i,255,100);
-    fill(i,i+20,i,20);
+    colorMode(RGB);
+    fill(c,a);
     ellipseMode(CENTER);
     ellipse(x*xspacing,width/2+yvalues[x],10,10);
   }
 }
-
+window.addCurve = addCurve;
+window.updateCurve = updateCurve;
+window.puls.init();
